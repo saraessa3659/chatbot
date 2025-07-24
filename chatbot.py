a08968dd6493e859144eb18d80435bd05da610a4
Sara Essa ❤️ <ganatrasara2010@gmail.com>
2025-07-24 13:01:06 +0500

    Update chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.title("Chatbot")
+st.write("This is a simple website for a chatbot")
+
+st.write("you can ask me anything, and i will try to respondto the best of my abilities")
